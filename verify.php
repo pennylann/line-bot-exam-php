01691da086ace84073c5522b69da7d337ca40599
pennylann <47096886+pennylann@users.noreply.github.com>
2019-07-05 13:35:25 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'G8bWXTdgWpjbjWGa95ZGgwulganRY7JNjC/OAZLf6B91XWrvDlen5Eb6Iy2UmNBUHVM/AgMU65e4Ewwv69kgkuFTs5zxki/B+LFLrw+lCFfuvBX21HzeFvJUFGHu/F1CFHblx5X8YHaPOSqAvPihSwdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
